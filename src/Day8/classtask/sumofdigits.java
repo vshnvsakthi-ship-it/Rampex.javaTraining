cdd55c69f32b64f091358a5bb14655a7dc438fee
Vaishnavi.S <vshnvsakthi@gmail.com>
2026-07-14 11:26:20 +0530

    day 8 class task

---
+++ src/Day8/classtask/sumofdigits.java
@@ -0,0 +1,16 @@
+package Day8.classtask;
+
+class sumofdigits {
+    public static void main(String[] args){
+        int num =543;
+        int sum=0;
+        while(num>0){
+            int digit=num%10;
+            sum+=digit;
+            num/=10;
+
+
+        }
+        System.out.print(sum);
+    }
+}
