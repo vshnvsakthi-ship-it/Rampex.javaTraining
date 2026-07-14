752c3bf77d0dee9bcc8bdfd8c3cdfe95d378dd02
Vaishnavi.S <vshnvsakthi@gmail.com>
2026-07-14 12:19:44 +0530

    Day 14 home task

---
+++ src/Day14/hometask/ChefExtendThread.java
@@ -0,0 +1,36 @@
+package Day14.hometask;
+
+class Chef extends Thread {
+    public void run() {
+        System.out.println(getName() + " : Making Noodles");
+        System.out.println(getName() + " : Preparing Fried Rice");
+        System.out.println(getName() + " : Cooking Soup");
+    }
+}
+class Waiter extends Thread {
+    public void run() {
+        System.out.println(getName() + " : Serving Table 1");
+        System.out.println(getName() + " : Serving Table 2");
+        System.out.println(getName() + " : Serving Table 3");
+    }
+}
+class Cashier extends Thread {
+    public void run() {
+        System.out.println(getName() + " : Generating Bill for Table 1");
+        System.out.println(getName() + " : Collecting Payment");
+        System.out.println(getName() + " : Printing Receipt");
+    }
+}
+class RestaurantUsingThread {
+    public static void main(String[] args) {
+        Chef chef = new Chef();
+        Waiter waiter = new Waiter();
+        Cashier cashier = new Cashier();
+        chef.setName("Chef Thread");
+        waiter.setName("Waiter Thread");
+        cashier.setName("Cashier Thread");
+        chef.start();
+        waiter.start();
+        cashier.start();
+    }
+}
