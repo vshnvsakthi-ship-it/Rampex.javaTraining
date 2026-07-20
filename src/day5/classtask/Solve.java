a03ced10a38c1e75b467f360ce31490739e39839
Vaishnavi.S <vshnvsakthi@gmail.com>
2026-07-20 14:07:39 +0530

    Day 5 classtask

---
+++ src/day5/classtask/Solve.java
@@ -0,0 +1,11 @@
+package day5.classtask;
+
+
+
+public class Solve {
+    public static void main(String[] args){
+        for(int i = 0; i < 7; i++) {
+            System.out.println( i);
+        }
+    }
+}
